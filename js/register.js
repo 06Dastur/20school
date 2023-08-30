2622da20cffb89838a42be3fe47afcdd48a0b16e
06Dastur <06dastur@gmail.com>
2023-08-30 10:08:45 +0300

    ikkinchi

--- js/register.js
+++ js/register.js
@@ -1,11 +1,6 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+let checkbox = document.getElementById("input[type='password']"),
+    input = document.getElementsByClassName("pass");
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
+checkbox.onclick = () => {
+  
+}
